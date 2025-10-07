5a9ad5dcc1ff34063a70103f733dc62c1754e2bc
Abdul-Kader-Rimon <abdulkaderrimon8@gmail.com>
2025-10-07 23:23:00 +0600

    skeletonLoader added

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -2,9 +2,10 @@ import React from 'react';
 import { Link} from 'react-router';
 import ProductCard from '../Componets/ProductCard';
 import useProducts from '../Hooks/useProducts';
+import SkeletonLoader from '../Componets/SkeletonLoader';
 
 const Home = () => {
-    const { products, loading, error } = useProducts();
+    const { products, loading } = useProducts();
     const featureProducts = products.slice(0,6)
     // console.log(products)
  
@@ -16,11 +17,15 @@ const Home = () => {
             See All Products
           </Link>
         </div>
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-          {featureProducts.map((product) => (
-            <ProductCard key={product.id} product={product} />
-          ))}
-        </div>
+        {loading ? (
+          <SkeletonLoader />
+        ) : (
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+            {featureProducts.map((product) => (
+              <ProductCard key={product.id} product={product} />
+            ))}
+          </div>
+        )}
       </div>
     );
 };
--- src/Pages/Products.jsx
+++ src/Pages/Products.jsx
@@ -1,9 +1,10 @@
 import React, { useState } from 'react';
 import useProducts from '../Hooks/useProducts';
 import ProductCard from '../Componets/ProductCard';
+import SkeletonLoader from '../Componets/SkeletonLoader';
 
 const Products = () => {
-    const { products } = useProducts();
+    const { products, loading } = useProducts();
     {/*search bar  */}
     const [search, setSearch] = useState("");
     const term = search.trim().toLocaleLowerCase();
@@ -21,8 +22,8 @@ const Products = () => {
             <span className="text-sm text-gray-500">
               ({searchedProducts.length}) Products Found.
             </span>
-            </h1>
-            {/*search bar  */}
+          </h1>
+          {/*search bar  */}
           <label className="input">
             <input
               value={search}
@@ -32,11 +33,15 @@ const Products = () => {
             />
           </label>
         </div>
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-          {searchedProducts.map((product) => (
-            <ProductCard key={product.id} product={product} />
-          ))}
-        </div>
+        {loading ? (
+          <SkeletonLoader count={16} />
+        ) : (
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+            {searchedProducts.map((product) => (
+              <ProductCard key={product.id} product={product} />
+            ))}
+          </div>
+        )}
       </div>
     );
 };
